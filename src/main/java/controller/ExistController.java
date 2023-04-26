e09e094be1513d0319e092d00e52e09cb7cf285f
Daniel88871 <druizl01@elpuig.xeill.net>
2023-04-27 01:39:41 +0200

    wiwiwi

--- src/main/java/controller/ExistController.java
+++ src/main/java/controller/ExistController.java
@@ -52,4 +52,46 @@ public class ExistController {
             throw new RuntimeException(e);
         }
     }
+
+    public void queryAllCampeones(){
+        XQResultSequence xqrs = executeQuery("for $campeon in doc('/db/apps/foaf/campeones.xml')//campeon return $campeon");
+        printResultSequence(xqrs);
+    }
+
+    public void queryAllCampeonesByName(){
+        XQResultSequence xqrs = executeQuery("for $campeon in doc('/db/apps/foaf/campeones.xml')//campeon order by $campeones/nombre return $campeones/nombre");
+        printResultSequence(xqrs);
+    }
+
+    public void modifierCampeonName(String oldName, String newName) {
+        String xquery = "update value \n" +
+                "doc('/db/apps/foaf/campeones.xml')//campeon[nombre='" + oldName + "']/nombre \n" +
+                " with '" + newName + "'";
+        executeCommand(xquery);
+
+        String query = "for $campeon in doc('/db/apps/foaf/campeones.xml')//campeon[nombre='" + newName + "'] return $campeon";
+        XQResultSequence xqrs = executeQuery(query);
+        printResultSequence(xqrs);
+    }
+
+    public void modifierVictoryPercentajeCampeon(String oldName, String newName) {
+        String xquery = "update value \n" +
+                "doc('/db/apps/foaf/campeones.xml')//campeon[porcentaje_de_victoria='" + oldName + "']/porcentaje_de_victoria \n" +
+                " with '" + newName + "'";
+        executeCommand(xquery);
+
+        String query = "for $campeon in doc('/db/apps/foaf/campeones.xml')//campeon[porcentaje_de_victoria='" + newName + "'] return $campeon";
+        XQResultSequence xqrs = executeQuery(query);
+        printResultSequence(xqrs);
+    }
+
+    public void deleteCampeonesByName(String nameCampeon){
+        String delete = "update delete doc('/db/apps/foaf/campeones.xml')//campeon[nombre='" + nameCampeon + "']";
+        executeCommand(delete);
+    }
+
+    public void deleteCampeonesByID(String IDCampeon){
+        String delete = "update delete doc('/db/apps/foaf/campeones.xml')//campeon[id='" + IDCampeon + "']";
+        executeCommand(delete);
+    }
 }
--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Menu {
@@ -18,24 +21,30 @@ public class Menu {
      * @return retorna las opciones
      */
     public int mainMenu() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        do {
 
         System.out.println("			          Menú  		            ");
         System.out.println("************************************************");
         System.out.println(" 	1. Mostrar todos los campeones	            ");
-        System.out.println("	2. Mostrar campeón por ID                   ");
-        System.out.println("	3. Modificar campeón                        ");
-        System.out.println("	4. Modificar el nombre de un campeón        ");
+        System.out.println("	2. Mostrar campeón por orden alfabético     ");
+        System.out.println("	3. Modificar el nombre de un campeón        ");
+        System.out.println("	4. Modificar el % de victoria de un campeón ");
         System.out.println("	5. Borrar campeones por ID de objeto        ");
         System.out.println("	6. Borrar un campeón por nombre             ");
         System.out.println("	0. Salir                         	        ");
         System.out.println("************************************************");
 
         System.out.println("Escoje una opción: ");
-        try{
-            option = sc.nextInt();
-        }catch (Exception e){
-            System.out.println("La opción elegida no existe, por favor, escoje una opción correcta!!");
+        try {
+            option = Integer.parseInt(br.readLine());
+        } catch (NumberFormatException | IOException e) {
+            System.out.println("Este valor no es válido, por favor escoja una opción correcta!!");
+            e.printStackTrace();
         }
+    } while (option != 1  && option != 2 && option != 3 && option != 4 && option != 5 && option != 6 && option != 0);
+
         return option;
     }
 }
